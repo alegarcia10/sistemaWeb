9d7ba6ff2f3e8fdcfec3c4a2c026b2b81a4bb934
alegarcia10 <alegerman610@gmail.com>
2022-08-09 22:27:50 -0300

    agrego gastos

--- application/views/admin/parteorden/vlist.php
+++ application/views/admin/parteorden/vlist.php
@@ -23,10 +23,10 @@
                     <div class="col-md-4">
                         <h4>Cliente: <?= $ordenindex->Nombre ?> </h4>
                     </div>
-                    <div class="col-md-2">
+                    <div class="col-md-4">
                         <h4>Gastos: $<?= $Gastos ?> </h4>
                     </div>
-                    <div class="col-md-2">
+                    <div class="col-md-4">
                         <h4>Monto a Facturar: $<?= $ordenindex->Precio ?> </h4>
                     </div>
                     
