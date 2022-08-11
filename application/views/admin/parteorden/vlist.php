d4c102ee29f22aa8690b97126bc6ff47ccc87235
alegarcia10 <alegerman610@gmail.com>
2022-08-11 13:00:58 -0300

    arreglo tamaño tabla tareas

--- application/views/admin/parteorden/vlist.php
+++ application/views/admin/parteorden/vlist.php
@@ -59,17 +59,17 @@
                                         <?php if (!empty($parteordenindex)) : ?>
                                             <?php foreach ($parteordenindex as $atributos) : ?>
                                                 <tr>
-                                                    <td><?php echo $atributos->IdParte; ?></td>
-                                                    <td><?php echo $atributos->TareaDesarrollada; ?></td>
-                                                    <td><?php if($atributos->FechaInicio == null){ echo "-";}else{echo $atributos->FechaInicio;} ?></td>
-                                                    <td><?php if($atributos->FechaFin == null){ echo "-";}else{echo $atributos->FechaFin;} ?></td>
-                                                    <td><?php if ($atributos->Completa == 1)
+                                                    <td width="10%"><?php echo $atributos->IdParte; ?></td>
+                                                    <td width="40%"><?php echo $atributos->TareaDesarrollada; ?></td>
+                                                    <td width="10%"><?php if($atributos->FechaInicio == null){ echo "-";}else{echo $atributos->FechaInicio;} ?></td>
+                                                    <td width="10%"><?php if($atributos->FechaFin == null){ echo "-";}else{echo $atributos->FechaFin;} ?></td>
+                                                    <td width="10%"><?php if ($atributos->Completa == 1)
                                                     { echo 'Completada';}elseif($atributos->Estado == 0)
                                                     { echo 'Pendiente';}elseif($atributos->Estado == 1)
                                                     { echo 'Recibida';}elseif($atributos->Estado == 2)
                                                     { echo 'En Curso';}elseif($atributos->Estado == 3)
                                                     { echo 'Finalizada';}else{ echo '-';} ; ?></td>
-                                                    <td>
+                                                    <td width="20%">
                                                         <div class="btn-group">
 
                                                             <a title="Modificar" href="<?php echo base_url(); ?>mantenimiento/cparteorden/cedit/<?php echo $atributos->IdParte; ?>" class="btn btn-info ">
