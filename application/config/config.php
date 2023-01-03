5cb48fda65aab17cc3ad50544b0f454d1a7d031f
gfonzali <fonzalidagonzalo@gmail.com>
2023-01-03 15:19:28 -0300

    fix ruta

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/proyectos/flor';
-$config['base_url'] = 'https://biosgastro.online/develop-sist';
+$config['base_url'] = 'https://biosgastro.online/featureuser-sist';
 //$config['base_url'] = 'https://biosgastro.online/master-sist';
 
 /*
