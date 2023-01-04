58e86ed9a3168221f30f424b8039d73711fe2b53
gfonzali <fonzalidagonzalo@gmail.com>
2023-01-04 11:46:49 -0300

    fix logo agregar tecnico

--- application/views/admin/parteorden/vedit.php
+++ application/views/admin/parteorden/vedit.php
@@ -75,7 +75,7 @@
 
                                <div class="col-sm-2">
                                  <br>
-                                 <button class="btn btn-success" type="button" id="buscar2"><span class="fa fa-search" aria-hidden="true" ></span> Agregar </button>
+                                 <button class="btn btn-success" type="button" id="buscar2"><span class="fa fa-plus" aria-hidden="true" ></span> Agregar </button>
                                </div>
                                <div class="col-sm-12 form-group">
                                      <table id="example1" class="table table-bordered table-hover order-table">
@@ -134,7 +134,7 @@
                           </div>
                           <div class="col-sm-2">
                             <br>
-                            <button class="btn btn-success" type="button" id="buscar1"><span class="fa fa-search" aria-hidden="true" ></span> Agregar </button>
+                            <button class="btn btn-success" type="button" id="buscar1"><span class="fa fa-plus" aria-hidden="true" ></span> Agregar </button>
                           </div>
                           <div class="col-sm-12 form-group">
                                 <table id="example1" class="table table-bordered table-hover order-table">
