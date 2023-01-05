bc89f3f9c186d89264b948aa994d2a05450de366
gfonzali <fonzalidagonzalo@gmail.com>
2023-01-05 16:40:55 -0300

    add

--- application/controllers/mantenimiento/Croles.php
+++ application/controllers/mantenimiento/Croles.php
@@ -1,71 +1,66 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cusuario extends CI_Controller {
+class Croles extends CI_Controller {
     function __construct(){
     parent:: __construct();
     if(!$this->session->userdata('login')){
         redirect(base_url());
     }
-    $this->load->model('musuario');
+    $this->load->model('mroles');
     $this->load->model('mcombo');
     }
 
 
 public function index(){
     $data = array (
-        'usuarioindex' => $this->musuario->mselectusuario(),
+        'rolesindex' => $this->mroles->mselectroles(),
     );
     $this->load->view('layouts/header');
     $this->load->view('layouts/aside');
-    $this->load->view('admin/usuario/vlist', $data);
+    $this->load->view('admin/roles/vlist', $data);
     $this->load->view('layouts/footer');
 }
 
 
 public function cadd(){
 
-    $data['tipo_usuario_select'] = $this->musuario->usuario_listar_select();
+    $data['tipo_usuario_select'] = $this->mroles->usuario_listar_select();
 
     $this->load->view('layouts/header');
     $this->load->view('layouts/aside');
-    $this->load->view('admin/usuario/vadd',$data);
+    $this->load->view('admin/roles/vadd',$data);
     $this->load->view('layouts/footer');
 }
 
 
 public function cinsert(){
 
-    $nombre = $this->input->post('txtnombre');
-     $email = $this->input->post('txtemail');
-     $idRol = $this->input->post('usuario');
-     $contraseña = $this->input->post('txtContraseña');
+     $nombre_tipo = $this->input->post('rol');
+     
 
-     $usu = $this->musuario->obtenerusuario($usuario);
+     $rol = $this->mroles->obtenerroles($roles);
 
-    if($usu==null){
+    if($rol==null){
 
         $data = array(
 
-            'nombre' => $nombre,
-            'email' => $email,
             'idRol' => $idRol,
-            'pass' => $contraseña,
-            'Anulado' => '0'
+            'nombre_tipo' => $nombre_tipo
         );
-        $res=$this->musuario->minsertusuario($data);
+        $res=$this->mroles->minsertroles($data);
         if($res){
             $this->session->set_flashdata('correcto', 'Se guardo Correctamente');
-            redirect(base_url().'mantenimiento/cusuario');
+            redirect(base_url().'mantenimiento/croles');
         }else{
             $this->session->set_flashdata('error', 'No se Guardo registro');
-            redirect(base_url().'mantenimiento/cusuario/cadd');
+            redirect(base_url().'mantenimiento/croles/cadd');
         }
 
     }else{
         //REGLA DE VALIDACION
-        $this->session->set_flashdata('error', "El Usuario '$usuario' ya esta registrado ");
-        redirect(base_url().'mantenimiento/cusuario/cadd');
+        $this->session->set_flashdata('error', "El Rol '$roles' ya está registrado ");
+        redirect(base_url().'mantenimiento/croles/cadd');
     }
 
     
@@ -75,50 +70,44 @@ public function cinsert(){
 
 public function cedit($id){
     $data = array(
-        'usuarioedit' => $this->musuario->midupdateusuario($id),
+        'rolesedit' => $this->mroles->midupdateroles($id),
     );
-    $data['usuario_select'] = $this->musuario->usuario_listar_select2();
-    $data['model'] = $this->musuario->obtener($data['usuarioedit']->idRol);
+    //$data['usuario_select'] = $this->musuario->usuario_listar_select2();
+    $data['model'] = $this->mroles->obtener($data['rolesedit']->idRol);
 
     $this->load->view('layouts/header');
     $this->load->view('layouts/aside');
-    $this->load->view('admin/usuario/vedit', $data);
+    $this->load->view('admin/roles/vedit', $data);
     $this->load->view('layouts/footer');
 }
 
 public function cupdate(){
 
-    $idusuario = $this->input->post('txtidusuario');
-    $nombre = $this->input->post('txtnombre');
-    $email = $this->input->post('txtemail');
-    $txtnombreviejo = $this->input->post('txtnombreviejo');
-    $idRol = mb_strtoupper($this->input->post('usuario'));
-    $contraseña = $this->input->post('txtContraseña');
+    $idRol = $this->input->post('txtidrol');
+    $nombre_tipo = $this->input->post('txtnombretipo');
 
-     $usu = $this->musuario->obtenerusuario($usuario);
+     $rol = $this->mroles->obtenerroles($roles);
 
-    if(($usu==null) or ($txtnombreviejo==$usuario) ){
+    if(($rol==null) or ($txtnombreviejo==$roles) ){
 
         $data = array(
-            'nombre' => $nombre,
-            'email' => $email,
             'idRol' => $idRol,
-            'pass' => $contraseña
+            'nombre_tipo' => $nombre_tipo
         );
 
-        $res = $this->musuario->mupdateusuario($idusuario, $data);
+        $res = $this->mrol->mupdateusuario($idRol, $data);
         if($res){
             $this->session->set_flashdata('correcto', 'Se Guardo Correctamente');
-            redirect(base_url().'mantenimiento/cusuario');
+            redirect(base_url().'mantenimiento/croles');
         }else {
-            $this->session->set_flashdata('error', 'No se pudo actualizar la usuario');
-            redirect(base_url().'mantenimiento/cusuario/cedit/'.$idusuario);
+            $this->session->set_flashdata('error', 'No se pudo actualizar el rol');
+            redirect(base_url().'mantenimiento/croles/cedit/'.$idRol);
         }
 
     }else{
         //REGLA DE VALIDACION
-        $this->session->set_flashdata('error', "El Usuario '$usuario' ya esta registrado ");
-        redirect(base_url().'mantenimiento/cusuario/cedit/'.$idusuario);
+        $this->session->set_flashdata('error', "El Rol '$roles' ya está registrado ");
+        redirect(base_url().'mantenimiento/croles/cedit/'.$idRol);
     }
 
 
@@ -130,12 +119,12 @@ public function cupdate(){
 
 public function cdelete($id){
 
-    $data=array(
+    /*$data=array(
         'Anulado' => '1'
-    );
-    $this->musuario->mupdateusuario($id, $data);
+    );*/
+    $this->mroles->mupdateroles($id);
     //redirect(base_url().'mantenimiento/cusuario');
-    echo "mantenimiento/cusuario";
+    echo "mantenimiento/croles";
 }
 
 
---
+++ application/models/Mroles.php
@@ -0,0 +1,85 @@
+<?php
+
+class Mroles extends CI_Model{
+
+   /* public function logeo ($user,$pass){
+
+        $this->db->where('nombre',$user);
+        $this->db->where('pass',$pass);
+        $resultado =$this->db->get('usuarios');
+        if($resultado->num_rows()>0){
+            return $resultado->row();
+        }
+        else{
+            return false;
+        }
+    }*/
+
+    //MOSTRAR usuario
+    public function mselectroles(){
+        //$this->db->where('Anulado =','0');
+        $resultado =$query=$this->db->query("SELECT t.nombre_tipo, t.idRol, u.idUsuario, u.nombre, u.email
+        FROM tipousuario t INNER JOIN usuarios u on t.idRol=u.idRol WHERE u.anulado = 0");
+        return $resultado->result();
+    }
+    //INSERTAR usuario
+    public function minsertroles($data){
+        return  $this->db->insert('roles',$data);
+    }
+
+    //OBTENER DATOS
+    public function midupdateroles($id){
+       $this->db->where('idRol', $id);
+       $resultado = $this->db->get('roles');
+       return $resultado->row();
+    }
+
+    //MODIFICAR usuario
+    public function mupdateroles($id, $data){
+        $this->db->where('idRol', $id);
+        return $this->db->update('roles', $data);
+     }
+     //Traer usuario
+    public function mselectinforoles($id){
+        $this->db->where('idRol =',"$id");
+        $resultado =$this->db->get('roles');
+        return $resultado->row();
+    }
+
+    // Listar tipo de usuario
+    public function roles_listar_select(){//
+        $query=$this->db->query("SELECT DISTINCT tipousuario.nombre_tipo as NOMBRE
+                                FROM tipousuario JOIN usuarios WHERE usuarios.anulado = 0");
+      return $query->result();
+      }
+
+    public function roles_listar_select2(){//
+        $query=$this->db->query("SELECT DISTINCT u.idUsuario, t.nombre_tipo
+                                FROM tipousuario t INNER JOIN usuarios u on t.idRol=u.idRol WHERE u.anulado = 1" );
+      return $query->result();
+      }
+
+      // Listar uno de tipo de usuario
+    public function roles_listar_select_uno($id){//
+        $query=$this->db->query("SELECT DISTINCT tipousuario.nombre_tipo as NOMBRE
+                                FROM tipousuario JOIN usuarios WHERE idusuario = $id");
+      return $query->result();
+      }
+
+
+      //Verifico si exite usuario
+    public function obtenerrol($rol){
+        $this->db->where('roles', $rol);
+        $resultado = $this->db->get('roles');
+        return $resultado->row();
+     }
+ 
+    function obtener($id){
+        $this->db->where("idRol",$id);
+        $query = $this->db->get('tipousuario');
+        return $query->row();
+        $error = $this->db->error();
+    }
+ 
+}
+?>
