1099fd18f69ea81ab19fca75e6f062b5d9aa4d17
alegarcia10 <alegerman610@gmail.com>
2023-01-17 20:45:20 -0300

    actualizo

--- application/views/admin/orden/vlist.php
+++ application/views/admin/orden/vlist.php
@@ -101,7 +101,9 @@
                                           <td width="15%"><?php echo $atributos->Nombre; ?></td>
                                           <td width="20%" >
                                                 <div class="btn-group">
-
+                                                    <a href="<?php echo base_url(); ?>mantenimiento/cparteorden/listar/<?php echo $atributos->IdOrden; ?>" class="btn btn-warning" title="Tareas">
+                                                        <span class="fa fa-eye"></span>
+                                                    </a>
                                                     <a href="<?php echo base_url(); ?>mantenimiento/corden/cedit/<?php echo $atributos->IdOrden; ?>" class="btn btn-info" title="Modificar">
                                                         <span class="fa fa-pencil"></span>
                                                     </a>
