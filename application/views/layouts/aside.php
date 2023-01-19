dcd6c6aa0dbd6fc041c887c295d5bafdcb3d4bee
gfonzali <fonzalidagonzalo@gmail.com>
2023-01-19 13:24:07 -0300

    fix aside

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -66,6 +66,7 @@
                             </span>
                             
                         </a>
+                        </li>
                 </ul>
                                 
             </section>
