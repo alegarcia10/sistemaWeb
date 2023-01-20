96abc1a398dec7a8921bc850adb57804045be21b
gfonzali <fonzalidagonzalo@gmail.com>
2023-01-20 10:55:46 -0300

    fix size button

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -30,7 +30,7 @@
                                   <input type="date" id="txtfechafin" name="txtfechafin" class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo set_value('txtfechafin') ?>">
                                 </div>
 
-                                <div class="col-sm-12 form-group">
+                                <div class="col-sm-2 form-group">
                                     <button type="submit" class="btn btn-success">Buscar</button>
                                 </div>
 
