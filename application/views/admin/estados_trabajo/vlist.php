e14572b040fb0d8988fc097e0fb6fbfb20a56350
gfonzali <fonzalidagonzalo@gmail.com>
2023-02-28 00:12:47 -0300

    fix

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -133,7 +133,7 @@ $(document).ready(function () {
                "language": {
                    "lengthMenu": "Mostrar _MENU_ registros por página",
                    "zeroRecords": "No se encontraron resultados en su búsqueda",
-                   "searchPlaceholder": "Buscar Órdenes Completas",
+                   "searchPlaceholder": "Buscar Completas",
                    "info": "Mostrando registros de _START_ al _END_ de un total de  _TOTAL_ registros",
                    "infoEmpty": "No existen registros",
                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
