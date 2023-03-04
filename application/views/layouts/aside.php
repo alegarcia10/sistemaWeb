286c95bda5b526797e7344929b8e7a0de2364919
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-03 21:18:15 -0300

    fx

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -16,7 +16,7 @@
                     <li class="treeview">
                         <a href="#">
                         <?php if ($roles->cliente =="1" || $roles->tecnico =="1" || $roles->ordenes =="1") { ?>
-                            <i class="fa fa-cogs"></i> <span>Menu</span>
+                            <i class="fa-solid fa-layer-group"></i> <span>Menu</span>
                             <span class="pull-right-container">
                                 <i class="fa fa-angle-left pull-right"></i>
                             </span>
@@ -41,7 +41,7 @@
                     <li class="treeview">
                         <a href="#">
                         
-                            <i class="fa-solid fa-lock"></i> <span>Administrador</span>
+                            <i class="fa-solid fa-lock"></i> <span> Administrador</span>
                             <span class="pull-right-container">
                                 <i class="fa fa-angle-left pull-right"></i>
                             </span>
@@ -49,10 +49,10 @@
                         </a>
                         <ul class="treeview-menu">
                         <?php if ($roles->usuarios =="1") { ?>
-                            <li><a href="<?php echo base_url(); ?>mantenimiento/cusuario"><i class="fa-solid fa-circle-user"></i>Usuarios</a></li>
+                            <li><a href="<?php echo base_url(); ?>mantenimiento/cusuario"><i class="fa-solid fa-circle-user"></i> Usuarios</a></li>
                         <?php } ?>
                         <?php if ($roles->roles =="1") { ?>    
-                            <li><a href="<?php echo base_url(); ?>mantenimiento/croles"><i class="fa-solid fa-key"></i>Roles</a></li>
+                            <li><a href="<?php echo base_url(); ?>mantenimiento/croles"><i class="fa-solid fa-key"></i> Roles</a></li>
                         <?php } ?>
                         </ul>
                     </li>
