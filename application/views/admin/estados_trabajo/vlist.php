cfc8c9e9d420ddca3013e5e63feb417b27ba5e1e
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-03 22:56:27 -0300

    fx

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -80,43 +80,7 @@
 </div>
 <style>
 
-.dt-button-collection .dt-button.buttons-columnVisibility {
-    background: none !important;
-    background-color: transparent !important;
-    box-shadow: none !important;
-    border: none !important;
-    padding: 0.25em 1em !important;
-    margin: 0 !important;
-    text-align: left !important;
-}
-.dt-button-collection .buttons-columnVisibility:before,
-.dt-button-collection .buttons-columnVisibility.active span:before {
-    display:block;
-    position:absolute;
-    top:1.2em;
-    left:0;
-    width:12px;
-    height:12px;
-    box-sizing:border-box;
-}
-.dt-button-collection .buttons-columnVisibility:before {
-    content:' ';
-    margin-top:-8px;
-    margin-left:10px;
-    border:1px solid black;
-    border-radius:3px;
-}
-.dt-button-collection .buttons-columnVisibility.active span:before {
-    font-family: 'Arial' !important;
-    content:'\2714';
-    margin-top: -15px;
-    margin-left: 12px;
-    text-align: center;
-    text-shadow: 1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff;
-}
-.dt-button-collection .buttons-columnVisibility span {
-    margin-left:17px;    
-}
+
 
 </style>
 
