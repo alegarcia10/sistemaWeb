6686dfbcd00c924e12fdd9d3b076b9cfa5fc8400
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-07 11:25:13 -0300

    fx ff

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -29,17 +29,17 @@
                       <form action="<?php echo base_url(); ?>" method="POST">
                       <h4>  &nbsp;&nbsp; &nbsp;Fechas</h4>          
                       <div class="col-md-10 form-group">
-                                <div class="col-sm-4 form-group">
+                                <div class="col-sm-3 form-group">
                                   
                                   <input type="date" id="txtfechaini" name="txtfechaini" class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo set_value('txtfechaini') ?>">
                                   </div>
                                 
-                                  <div class="col-sm-4 form-group">
+                                  <div class="col-sm-3 form-group">
                                   <input type="date" id="txtfechafin" name="txtfechafin" class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo set_value('txtfechafin') ?>">
                                   </div>
                                 
                                 
-                                  <div class="col-sm-3 form-group">
+                                  <div class="col-sm-5 form-group">
                                     <button type="submit" class="btn btn-success">Buscar</button> 
                                     </div>
                         </div>      
