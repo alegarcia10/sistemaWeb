98e36db9659fb50a5cbf2fe0819146fcf1857932
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-07 18:51:15 -0300

    fx

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -105,7 +105,7 @@
         .dt-button.buttons-columnVisibility.active {
               background: white !important;
               color: black !important;
-              border-color: red !important;
+              border-color: blue !important;
               
            }
 
