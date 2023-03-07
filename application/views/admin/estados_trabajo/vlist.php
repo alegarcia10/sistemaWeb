9b416616908f012383fd29b099599e6cc0348bf6
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-07 11:11:42 -0300

    fx ff

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -26,14 +26,14 @@
                     <div class="col-md-12">
                         <div class="col-md-12">
                         </div>
-                      <form action="</*?php echo base_url(); ?>" method="POST">
+                      <form action="<?php echo base_url(); ?>" method="POST">
                                 <div class="col-sm-2 form-group">
                                   <label for="fechaini">Inicio</label>
-                                  <input type="date" id="txtfechaini" name="txtfechaini" class="form-control" min="2020-01-01" max="2100-12-31" value="</**?php echo set_value('txtfechaini') ?>">
+                                  <input type="date" id="txtfechaini" name="txtfechaini" class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo set_value('txtfechaini') ?>">
                                 </div>
                                 <div class="col-sm-2 form-group">
                                   <label for="fechafin">Fin</label>
-                                  <input type="date" id="txtfechafin" name="txtfechafin" class="form-control" min="2020-01-01" max="2100-12-31" value="</**?php echo set_value('txtfechafin') ?>">
+                                  <input type="date" id="txtfechafin" name="txtfechafin" class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo set_value('txtfechafin') ?>">
                                 </div>
                                 <br>
                                 <div class="col-sm-6 form-group">
@@ -164,7 +164,7 @@ $(document).ready(function () {
                     text: '<i class="fa fa-print"></i>',
                     titleAttr: 'Imprimir',
                     className: 'btn btn-info'
-                },
+                }
                 
                
             ],
