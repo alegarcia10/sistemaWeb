4958f556dc6000dc6fbbff17a18b003b70082a5d
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-07 10:53:40 -0300

    fx ff

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -22,6 +22,25 @@
                         <div class="col-md-12">
                           <h3>Listado Trabajos</h3>
                         </div>
+                        <div class="row">
+                    <div class="col-md-12">
+                        <div class="col-md-12">
+                          <h2>Filtros</h2>
+                        </div>
+                      <form action="</*?php echo base_url(); ?>" method="POST">
+                                <div class="col-sm-2 form-group">
+                                  <label for="fechaini">Inicio</label>
+                                  <input type="date" id="txtfechaini" name="txtfechaini" class="form-control" min="2020-01-01" max="2100-12-31" value="</**?php echo set_value('txtfechaini') ?>">
+                                </div>
+                                <div class="col-sm-2 form-group">
+                                  <label for="fechafin">Fin</label>
+                                  <input type="date" id="txtfechafin" name="txtfechafin" class="form-control" min="2020-01-01" max="2100-12-31" value="</**?php echo set_value('txtfechafin') ?>">
+                                </div>
+                                <br>
+                                <div class="col-sm-10 form-group">
+                                    <button type="submit" class="btn btn-success">Buscar</button>
+                                </div>
+                            </form>
                         <table id="tablaordenc" class="table table-bordered table-hover order-table1">
                             <thead>
                                 <tr>
