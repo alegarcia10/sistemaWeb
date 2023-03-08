109fa51143bef3f45502126dd419cd88dde4c13b
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-08 20:11:15 -0300

    fc

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -93,7 +93,7 @@
                                             <td width="8%">$<?php echo  number_format($atributos->Ganancia, 2); ?></td>
                                             <td width="8%"><?php echo number_format($atributos->HH, 2);?></td>
                                             <td width="8%">$<?php echo '-'//$atributos->rentabilidad; ?></td>
-                                            <td width="8%"><?php echo '<input type="text" name="r1_age" value="" size="3"' ?></td>
+                                            <td width="8%"><?php echo '<input type="text" name="r1_age" value="" ' ?></td>
                                             <td width="8%"><?php echo '<input type="text" name="r1_age" value="" size="3"' ?></td>
                                             <td width="8%"><?php echo '<input type="text" name="r1_age" value="" size="3"' ?></td>
                                             <td width="8%"><?php echo '<input type="text" name="r1_age" value="" size="3"' ?></td>
