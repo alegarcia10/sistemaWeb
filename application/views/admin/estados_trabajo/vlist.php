cd69e045859cbbc7a28c30309b7384f483d738c3
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-08 14:35:22 -0300

    fx

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -119,7 +119,7 @@
 
            div.dataTables_wrapper div.dataTables_info{
             position:absolute;
-            bottom: -150px;
+            bottom: -100px;
             /*float:right;*/
            }
            
