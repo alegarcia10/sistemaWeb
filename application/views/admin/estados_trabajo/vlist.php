b05404654a0d81e242cd25c7ee3843a935ebd54c
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-10 01:00:33 -0300

    fxc

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -26,7 +26,7 @@
                                   <input type="date" id="txtfechaf" name="txtfechaf" class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo set_value('txtfechaf') ?>">
                                   <a href="<?php echo base_url(); ?>mantenimiento/ctrabajos/indexFiltro" class="btn btn-success">Buscar</a> 
                                 </div>
-                                
+                                 
                      </div> 
                        </form>
                     
