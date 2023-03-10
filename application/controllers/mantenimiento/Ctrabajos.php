afc4e3421fb0ff354139b62c2c17ef90c7e5a6e7
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-10 12:25:58 -0300

    dfc

--- application/controllers/mantenimiento/Ctrabajos.php
+++ application/controllers/mantenimiento/Ctrabajos.php
@@ -286,9 +286,7 @@ public function indexFiltro(){
     $this->load->view('admin/estados_trabajo/vlist',$data);
     $this->load->view('layouts/footer');
     }//fin if
-    else{
-        index();
-    }
+    
 }
 
 
