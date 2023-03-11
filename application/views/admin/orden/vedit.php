9a07ed3a657bdc81d2806188dcfb0855736a7f27
alegarcia10 <alegerman610@gmail.com>
2023-03-11 17:29:15 -0300

    asd

--- application/views/admin/orden/vedit.php
+++ application/views/admin/orden/vedit.php
@@ -11,7 +11,7 @@
                <hr>
                <div class="row">
                    <div class="col-md-12">
-                       <?php if($this->session->flashdata('error')):?>
+                       <?php var_dump($facturas); die; if($this->session->flashdata('error')):?>
                         <div class="alert alert-danger">
                             <p><?php echo $this->session->flashdata('error') ?> </p>
                         </div>
