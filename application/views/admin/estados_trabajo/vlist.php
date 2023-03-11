46347ad61687b0e07baa406c1b2afc7791ff74e8
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-11 16:25:59 -0300

    fc

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -97,11 +97,11 @@
                                             <td width="8%"><?php echo number_format($atributos->rentabilidad,2); ?>%</td>
                                             <td width="8%"><?php if ($atributos->N_factura != null)
                                                     { echo $atributos->N_factura;}else{ echo '-';} ; ?></td>
-                                            <td width="8%"><?php if ($atributos->fecha_factura != null)
+                                            <td width="8%"><?php if ($atributos->fecha_factura != "-")
                                                     { echo date("d-m-Y", strtotime("$atributos->fecha_factura"));}else{ echo '-';} ; ?></td>
                                             <td width="8%"><?php if ($atributos->estado_pago != null)
                                                     { echo ($atributos->estado_pago);}else{ echo '-';} ; ?></td>
-                                            <td width="8%"><?php if ($atributos->fecha_pago != null)
+                                            <td width="8%"><?php if ($atributos->fecha_pago != "-")
                                                     { echo date("d-m-Y", strtotime("$atributos->fecha_pago"));}else{ echo '-';} ; ?></td>
                                             <td width="8%"><?php echo $atributos->demora; ?></td>
                                             
