e06899d922e9cbc0c4ff3b172efe6562f0712a09
gfonzali <fonzalidagonzalo@gmail.com>
2023-03-22 12:38:58 -0300

    fc

--- application/views/admin/estados_trabajo/vlist.php
+++ application/views/admin/estados_trabajo/vlist.php
@@ -86,7 +86,7 @@
                                                 <tr>
                                                     
                                                     <td align="center" width="10%"><?php if ($atributos->Fecha != "-") 
-                                                                        {echo substr("$atributos->Fecha", 0, 10);}
+                                                                        {echo "$atributos->Fecha";}
                                                                         else{echo '-' ; };?></td>
                                                     <td width="15%"><?php echo $atributos->Nombre; ?></td>
                                                     <td width="24%"><?php echo $atributos->TareaDesarrollar; ?></td>
