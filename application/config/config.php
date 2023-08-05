6e6c44ec1b648d4dc127476138fcde26f831539c
Gonzalo Fonzalida <50595704+fonzalida@users.noreply.github.com>
2023-08-04 23:31:30 -0300

    fix configs

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/proyectos/flor';
-$config['base_url'] = 'https://biosgastro.online/feature-sist';
+$config['base_url'] = 'https://biosgastro.online/gonza-sist';
 //$config['base_url'] = 'https://biosgastro.online/master-sist';
 
 /*
