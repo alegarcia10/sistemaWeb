97ad1131a71ddd3f0646f03a9280baf86be7a06e
Gonzalo Fonzalida <50595704+fonzalida@users.noreply.github.com>
2023-08-05 04:03:43 +0000

    fix

--- application/controllers/mantenimiento/Cequipos.php
+++ application/controllers/mantenimiento/Cequipos.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Ccliente extends CI_Controller {
+class Cequipos extends CI_Controller {
     function __construct(){
     parent:: __construct();
     if(!$this->session->userdata('login')){
