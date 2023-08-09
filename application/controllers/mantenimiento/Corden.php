3e41654aa51a0736234af6419b2ab24b855ca815
gfonzali <fonzalidagonzalo@gmail.com>
2023-08-09 11:32:19 -0300

    fc

--- application/controllers/mantenimiento/Corden.php
+++ application/controllers/mantenimiento/Corden.php
@@ -200,7 +200,7 @@ public function cupdatefact(){
     $res=$this->mfactura->midupdatefact($nfact);
     $fac=$this->mfactura->mbuscaordenfactura($idorden);
     if(($fac==null) or ($fac!=$id)){
-       if(($res==null)){
+       if(($res==null) or ($nfact==$id)){
           
           $data = array(
               'N_factura' => $nfact,
