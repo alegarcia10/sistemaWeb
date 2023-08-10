5efd4adf01fa5bc628fd104e3e986748a8fbadfc
gfonzali <fonzalidagonzalo@gmail.com>
2023-08-10 18:32:32 -0300

    fixs

--- application/views/admin/recepcion_equipos/vedit.php
+++ application/views/admin/recepcion_equipos/vedit.php
@@ -20,7 +20,7 @@
                         <input type="hidden" value="<?php echo $equiposedit->num_orden ?>" name="txtnumorden" id="txtnumorden"> 
                         <div class="col-sm-2 form-group">
                                   <label for="fecha">Fecha</label>
-                                  <input type="date" id="txtfecha" name="txtfecha" min="1"  class="form-control" value="<?php echo set_value('txtfecha') ?>" required >
+                                  <input type="date" id="txtfecha" name="txtfecha" min="1"  class="form-control" min="2020-01-01" max="2100-12-31" value="<?php echo !empty(form_error('txtfecha'))? set_value('txtfecha') :  date("d-m-Y", strtotime("$equiposedit->fecha"));?>" required >
                                 </div>
                                 <div class="col-md-5 form-group">
                               <label for="cliente">Cliente&nbsp;&nbsp; (*)</label>
@@ -31,19 +31,19 @@
 
                                 <div class="col-sm-6 form-group">
                                     <label for="marca">Marca</label>
-                                    <input type="text" id="txtmarca" name="txtmarca"maxlength="1000" class="form-control" value="<?php echo set_value('txtmarca') ?>"  >
+                                    <input type="text" id="txtmarca" name="txtmarca"maxlength="1000" class="form-control" value="<?php echo !empty(form_error('txtmarca'))? set_value('txtmarca') : $equiposedit->marca ?>">
                                 </div>
                                 <div class="col-sm-6 form-group">
                                     <label for="modelo">Modelo</label>
-                                    <input type="text" id="txtmodelo" name="txtmodelo" maxlength="950"class="form-control" value="<?php echo set_value('txtmodelo') ?>"  >
+                                    <input type="text" id="txtmodelo" name="txtmodelo" maxlength="950"class="form-control" value="<?php echo !empty(form_error('txtmodelo'))? set_value('txtmodelo') : $equiposedit->modelo ?>">
                                 </div>
                                 <div class="col-sm-6 form-group">
                                     <label for="serie">Número serie</label>
-                                    <input type="text" id="txtserie" name="txtserie" maxlength="950"class="form-control" value="<?php echo set_value('txtserie') ?>"  >
+                                    <input type="text" id="txtserie" name="txtserie" maxlength="950"class="form-control" value="<?php echo !empty(form_error('txtserie'))? set_value('txtserie') : $equiposedit->num_serie ?>">
                                 </div>
                                 <div class="col-sm-6 form-group">
                                     <label for="sector">Sector</label>
-                                    <input type="text" id="txtsector" name="txtsector" maxlength="950"class="form-control" value="<?php echo set_value('txtsector') ?>"  >
+                                    <input type="text" id="txtsector" name="txtsector" maxlength="950"class="form-control" value="<?php echo !empty(form_error('txtsector'))? set_value('txtsector') : $equiposedit->sector ?>"  >
                                 </div>
                                 <div class="col-sm-6 form-group">
                                     <label for="descripcion">Descripción</label>
