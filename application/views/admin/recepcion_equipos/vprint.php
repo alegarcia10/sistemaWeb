28b61ee1e3f1e69cc23545a192a7fbb4c67d59ea
gfonzali <fonzalidagonzalo@gmail.com>
2023-08-10 19:16:34 -0300

    fc

--- application/views/admin/recepcion_equipos/vprint.php
+++ application/views/admin/recepcion_equipos/vprint.php
@@ -37,9 +37,9 @@
                             <div class="customer-equipment">
                                 <div class="customer">
                                     <h2>Datos del Cliente</h2>
-                                    <p>Cliente: </p>
-                                    <p>Domicilio: </p>
-                                    <p>Teléfono: </p>
+                                    <p>Cliente: <?php echo $equiposindex->Nombre; ?></p>
+                                    <p>Domicilio: <?php echo $equiposindex->Domicilio." ".$equiposindex->Localidad." ".$equiposindex->Provincia; ?></p>
+                                    <p>Teléfono: <?php echo $equiposindex->Telefono1; ?></p>
                                     <p>Correo Electrónico: </p>
                                 </div>
                                 <div class="equipment">
