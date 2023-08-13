1f24ccba7e2dc92e29df2451727f04b182dda797
alegarcia10 <alegerman610@gmail.com>
2023-08-13 20:09:05 -0300

    .

--- application/controllers/mantenimiento/Cremitos.php
+++ application/controllers/mantenimiento/Cremitos.php
@@ -100,8 +100,7 @@ public function cedit($id){
     $data['cliente_select'] = $this->mremito->cliente_listar_select2();
     
     $data['model'] = $this->mremito->obtener($data['remitoedit']->IdCliente);
-    var_dump($data);
-    die;   
+    
     //$roles=$this->mroles->obtener($idRol);
     $this->load->view('layouts/header');
     $this->load->view('layouts/aside',$data);
