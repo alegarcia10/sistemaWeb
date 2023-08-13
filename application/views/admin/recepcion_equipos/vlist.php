57c528ff8d00813e794975908f8fc826fb86a719
gfonzali <fonzalidagonzalo@gmail.com>
2023-08-13 13:31:32 -0300

    fc

--- application/views/admin/recepcion_equipos/vlist.php
+++ application/views/admin/recepcion_equipos/vlist.php
@@ -79,7 +79,7 @@ $(document).ready(function () {
                "language": {
                    "lengthMenu": "Mostrar _MENU_ registros por página",
                    "zeroRecords": "No se encontraron resultados en su búsqueda",
-                   "searchPlaceholder": "Buscar Cliente",
+                   "searchPlaceholder": "Buscar Orden",
                    "info": "Mostrando registros de _START_ al _END_ de un total de  _TOTAL_ registros",
                    "infoEmpty": "No existen registros",
                    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
