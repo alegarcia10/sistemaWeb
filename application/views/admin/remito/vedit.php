2cfd4329b77e5372e5bcb38826c5b1c319ea99a1
alegarcia10 <alegerman610@gmail.com>
2023-08-14 01:52:45 -0300

    .

--- application/views/admin/remito/vedit.php
+++ application/views/admin/remito/vedit.php
@@ -101,19 +101,19 @@ $(document).ready(function(){
         $('#txtproducto').val('');
         $('#txtcantidad').val('');
         $('#txtprecio').val('');
-        alert("llega");
+       
                 $.ajax( {
                                     method:'POST',
                                     url:'<?php echo base_url(); ?>' + 'mantenimiento/cremitos/addProducto',
                                     dataType:'html',
                                     data:{producto:producto,idRemito:idRemito,cant:cant,precio:precio}})
-                                    alert("llega0");
+                                   
                                     .done(function(r) {
 
                                     r = JSON.parse(r);
-                                    alert("llega1");
+                                  
                                     window.location.href=base_url+'/mantenimiento/cremitos/cedit/'+idRemito;
-                                    alert("llega2");
+                                
                                     //$("#tbody1").append(r['linksa']);
                                     });
 
