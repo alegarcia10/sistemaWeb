6a8c37a736d3e9616020eb79d516caa863d3328c
alegarcia10 <alegerman610@gmail.com>
2023-08-14 01:54:33 -0300

    .

--- application/views/admin/remito/vedit.php
+++ application/views/admin/remito/vedit.php
@@ -10,6 +10,39 @@
             <div class="box-body">
                <hr>
                <div class="row">
+                   <div class="col-md-12">
+                       <?php if($this->session->flashdata('error')):?>
+                        <div class="alert alert-danger">
+                            <p><?php echo $this->session->flashdata('error') ?> </p>
+                        </div>
+                        <?php endif ; ?>
+                        <form action="<?php echo base_url();?>mantenimiento/cremitos/cupdate" method="POST">
+                            <input type="hidden" value="<?php echo $remitoedit->IdRemito?>" name="txtIdRemito" id="txtIdRemito">
+                            <div class="col-md-5 form-group">
+                              <label for="cliente">Cliente&nbsp;&nbsp; (*)</label>
+                							<? $this->select_items->sin_buscador2($cliente_select,(!empty($model->IdCliente))
+                               ? $model->IdCliente : '',	'cliente','1',(!empty($consultar)) ? "disabled ":'required');?>
+                			<input id="cliente_hidden" name="cliente_hidden" type="hidden" >
+                			</div>
+                            <div class="col-sm-3 form-group">
+                                <label for="fecha">Fecha de Recepción</label>
+                                <input type="text" id="txtfecha" name="txtfecha"  min="2020-01-01" max="2100-12-31" value="<?php echo !empty(form_error('txtfecha'))? set_value('txtfecha') :  date("d-m-Y", strtotime("$remitoedit->fecha"));?>" class= "form-control"   >
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label for="vendedor">VENDEDOR</label>
+                                <input type="text" id="txtvendedor" name="txtvendedor" maxlength="256" value="<?php echo !empty(form_error('txtvendedor'))? set_value('txtvendedor') : $remitoedit->vendedor ?>" class= "form-control" required >
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label for="observaciones">OBSERVACIONES</label>
+                                <input type="text" id="txtobservaciones" name="txtobservaciones" maxlength="1000" value="<?php echo !empty(form_error('txtobservaciones'))? set_value('txtobservaciones') : $remitoedit->observaciones ?>" class= "form-control"  >
+                            </div>
+                        
+                            <div class="col-sm-6 form-group">
+                                <a class="btn btn-success" href="<?php echo base_url();?>mantenimiento/cremitos">Volver</a>
+                                <button type="submit" class="btn btn-success">Guardar</button>
+                            </div>
+                        </form>
+                    </div>
                     <div class="col-md-12">
                         <div class="row">
                         <?php if($this->session->flashdata('error')):?>
@@ -86,6 +119,8 @@
 <script type="text/javascript">
 
 $(document).ready(function(){
+
+    var base_url= "<?php echo base_url();?>";
   $('#agregarProducto').on('click',function(){
 
         var idRemito =$('#txtIdRemito').val();
