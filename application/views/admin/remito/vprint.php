0e4fa3838d73be27bceca93f9b973f249929a159
alegarcia10 <alegerman610@gmail.com>
2023-08-21 23:51:27 -0300

    .

--- application/views/admin/remito/vprint.php
+++ application/views/admin/remito/vprint.php
@@ -90,7 +90,7 @@
   .header {
     display: flex;
     align-items: center;
-    margin-bottom: -203px;
+    margin-bottom: -303px;
     flex-direction: row;
     flex-wrap: nowrap;
     align-content: stretch;
