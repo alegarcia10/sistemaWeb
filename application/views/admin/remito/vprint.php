0c6f228033b5741c781799fce69a9c9ad3e8ee6c
alegarcia10 <alegerman610@gmail.com>
2023-08-21 21:03:44 -0300

    .

--- application/views/admin/remito/vprint.php
+++ application/views/admin/remito/vprint.php
@@ -107,7 +107,6 @@
         border: 1px solid #ccc;
         box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
         display: flex;
-        flex-direction: column;
         justify-content: space-between;
     }
 
