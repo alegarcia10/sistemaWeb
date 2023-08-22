7bd272dab9e13c9c2797e3257f31ee2774854c43
alegarcia10 <alegerman610@gmail.com>
2023-08-22 12:56:46 -0300

    s

--- application/views/admin/remito/vprint.php
+++ application/views/admin/remito/vprint.php
@@ -30,6 +30,14 @@
                                             <p>COMPROBANTE NO VALIDO COMO FACTURA</p>
                                             <p>Fecha: <?php echo date("d/m/Y", strtotime("$remito->fecha")); ?></p>
                                         </div>
+                                        <div class="col-md-6 datos3">
+                                          <p>Cereseto Oeste 156</p>
+                                          <p>Teléfono: 264-4275852</p>
+                                        </div>
+                                        <div class="col-md-6 datos3">
+                                            <p>C.U.I.T: 30-71621552-7 </p>
+                                            <p>Inicio de Actividades: 01/10/2018 </p>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="col-md-12" id="celdas3">
