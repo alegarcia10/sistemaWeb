727657816268e6e921fb54bca52c77bc845920ec
alegarcia10 <alegerman610@gmail.com>
2023-08-22 00:13:01 -0300

    .

--- application/views/admin/remito/vprint.php
+++ application/views/admin/remito/vprint.php
@@ -47,34 +47,31 @@
                             <div class="divider"></div>
                             <div class="description">
                                 <table class="table">
-                                    <thead class="thead-dark">
-                                        <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="table-primary">
-                                        <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                        </tr>
-                                    </tbody>
+                                    <!-- On rows -->
+                                    <tr class="table-active">...</tr>
+
+                                    <tr class="table-primary">...</tr>
+                                    <tr class="table-secondary">...</tr>
+                                    <tr class="table-success">...</tr>
+                                    <tr class="table-danger">...</tr>
+                                    <tr class="table-warning">...</tr>
+                                    <tr class="table-info">...</tr>
+                                    <tr class="table-light">...</tr>
+                                    <tr class="table-dark">...</tr>
+
+                                    <!-- On cells (`td` or `th`) -->
+                                    <tr>
+                                    <td class="table-active">...</td>
+
+                                    <td class="table-primary">...</td>
+                                    <td class="table-secondary">...</td>
+                                    <td class="table-success">...</td>
+                                    <td class="table-danger">...</td>
+                                    <td class="table-warning">...</td>
+                                    <td class="table-info">...</td>
+                                    <td class="table-light">...</td>
+                                    <td class="table-dark">...</td>
+                                    </tr>
                                 </table>
                             </div>
                             
