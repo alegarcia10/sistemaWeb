91b36517a51455fbf5c2ea35d763cc0a29851025
alegarcia10 <alegerman610@gmail.com>
2023-08-22 12:15:28 -0300

    .

--- application/views/admin/remito/vprint.php
+++ application/views/admin/remito/vprint.php
@@ -193,11 +193,11 @@
   @media print {
   
     .cuerpo {
-    width: 150mm; /* Ancho A4 */
+    width: 180mm; /* Ancho A4 */
     height: 250mm; /* Alto A4 */
     margin: auto;
     margin-top: 10px !important;
-    padding: 25px;
+    padding: auto;
     border: 1px solid #ccc;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
   }
