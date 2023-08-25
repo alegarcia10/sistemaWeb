9202fa29c315fc1501afcb51a500f280e4ee5ab1
alegarcia10 <alegerman610@gmail.com>
2023-08-25 18:26:26 -0300

    asd

--- application/views/admin/remito/vprint.php
+++ application/views/admin/remito/vprint.php
@@ -405,7 +405,7 @@
     background-color: black !important;
     font-size: 30px;
     padding: 0px !important;
-    margin-top: -9.5% !important;
+    margin-top: -11.5% !important;
     margin-left: 47.5% !important;
     height: 65px !important;
     width: 5%;
