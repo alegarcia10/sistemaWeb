46cf3474bc8e3e9476f965533922eed74ed9dffa
gfonzali <fonzalidagonzalo@gmail.com>
2023-08-25 16:45:50 -0300

    add remitos

---
+++ application/models/Mremito.php
@@ -0,0 +1,169 @@
+<?php
+
+class Mremito extends CI_Model{
+
+    //MOSTRAR remito activas
+
+  public function mselectremito(){
+
+    $resultado =	$query = $this->db->query("SELECT o.IdRemito , c.Nombre ,o.IdCliente, o.fecha ,
+       o.observaciones, o.vendedor  FROM remitos o
+       INNER JOIN cliente c ON o.IdCliente = c.IdCliente 
+       where o.Anulado=0 
+       ORDER BY o.IdRemito DESC;");
+    return $resultado->result();
+
+}
+
+ 
+
+    public function mselectremitofecha($ini,$fin){
+     
+
+      $ini = date("Y-m-d", strtotime($ini));
+      $fin = date("Y-m-d", strtotime($fin));
+     
+      $resultado =	$query = $this->db->query("SELECT o.IdRemito , c.Nombre ,o.IdCliente, o.FechaRecepcion ,
+         o.TareaDesarrollar, o.Precio, o.Completada, o.Eliminada FROM remito o
+         INNER JOIN cliente c ON o.IdCliente = c.IdCliente 
+         where o.Eliminada=0 and o.Completada=0 and o.FechaRecepcion >= '$ini' and o.FechaRecepcion <= '$fin' 
+         ORDER BY o.IdRemito DESC;");
+    
+      return $resultado->result();
+      
+
+  }
+
+    public function consultaTareas($id){
+        $this->db->where('IdRemito', $id);
+        $resultado=$this->db->get('remitos');
+        return  $resultado->result();
+     }
+
+     //CALCULA TOTAL ACUMULADO A COBRAR EN REMITO
+     public function consultaTotalRemito($id){
+       $resultado =$query = $this->db->query("SELECT SUM(precio)as totalPrecio FROM producto where IdRemito=$id");
+      
+        $resultado=$resultado->row();
+       $totalPrecio=$resultado->totalPrecio;
+
+       $x=intval($totalPrecio);
+     
+         return  $totalPrecio;
+      }
+
+
+     
+    //MOSTRAR remito completas
+   
+    //INSERTAR remito
+    public function minsertremito($data){
+        return  $this->db->insert('remitos',$data);
+    }
+
+    //OBTENER DATOS con IdRemito
+    public function midupdateremito($id){
+       $this->db->where('IdRemito', $id);
+       $resultado = $this->db->get('remitos');
+       return $resultado->row();
+    }
+
+    
+  
+    //MODIFICAR remito
+    public function mupdateremito($id, $data){
+        $this->db->where('IdRemito', $id);
+        return $this->db->update('remitos', $data);
+     }
+     //Traer remito
+    public function mselectinforemito($id){
+        $this->db->where('IdRemito =',"$id");
+        $resultado =$this->db->get('remitos');
+        return $resultado->row();
+    }
+
+    public function cliente_listar_select(){//
+      $query=$this->db->query("SELECT DISTINCT cliente.IdCliente  ID ,cliente.Nombre as NOMBRE
+                              FROM cliente WHERE cliente.Anulado = 0
+                              ORDER BY cliente.Nombre ASC " );
+    return $query->result();
+    }
+    public function cliente_listar_select2(){//
+      $query=$this->db->query("SELECT DISTINCT cliente.IdCliente  IdCliente ,cliente.Nombre as NOMBRE
+                              FROM cliente WHERE cliente.Anulado = 0
+                              ORDER BY cliente.Nombre ASC " );
+    return $query->result();
+    }
+    function obtener($id){//
+  		$this->db->where("IdCliente",$id);
+  		$query = $this->db->get('cliente');
+  		return $query->row();
+  		$error = $this->db->error();
+  	}
+   
+    //Trear PRUDCTOS DE REMITOS
+    public function obtenerProducto($IdRemito){
+      $this->db->where('IdRemito =',"$IdRemito");
+      $resultado =$this->db->get('producto');
+      return $resultado->result();
+  }
+
+  public function cargarProd($data){
+
+    $IdRemito=$data['IdRemito'];
+    $cantidad=$data['cantidad'];
+    $producto=$data['producto'];
+    $precio=$data['precio'];
+    $this->db->where('IdRemito =',"$IdRemito");
+    $this->db->insert('producto',$data);
+    $IdMat=$this->db->insert_id();
+
+    return $linka= '<tr>
+                <td>'.$IdProducto.'</td>
+                <td>'.$producto.'</td>
+                <td>'.$cantidad.'</td>
+                <td>'.$precio.'</td>
+                <td>
+                    <div class="btn-group">
+                        <a title="Modificar" href="@'.$IdProducto.'" class="btn btn-info ">
+                            <span class="fa fa-pencil"></span>
+                        </a>
+                        <a title="Eliminar" href="_'.$IdProducto.'" class="btn btn-danger btn-remove">
+                            <span class="fa fa-remove"></span>
+                        </a>
+                    </div>
+                </td>
+            </tr>';
+}
+
+//Trear Producto con IdProducto
+public function obtenerProdconIdProd($IdProducto){
+
+  $this->db->where('IdProducto =',"$IdProducto");
+  $resultado =$this->db->get('producto');
+  return $resultado->row();
+}
+
+//Eliminar Producto
+public function mdeleteproducto($IdProducto){
+
+  $this->db->where('IdProducto =',"$IdProducto");
+  $resultado =$this->db->delete('producto');
+
+}
+
+//OBTENER DATOS Producto
+public function midupdateproducto($id){
+  $this->db->where('IdProducto', $id);
+  $resultado = $this->db->get('producto');
+  return $resultado->row();
+}
+
+//MODIFICAR producto
+public function mupdateproducto($id, $data){
+  $this->db->where('IdProducto', $id);
+  return $this->db->update('producto', $data);
+}
+    
+}
+?>
