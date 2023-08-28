7e37bf4ddf8c644a7a7b4e89ee6ebec2de4a65bd
alegarcia10 <alegerman610@gmail.com>
2023-08-28 18:07:05 -0300

    asd

--- application/views/admin/remito/vedit.php
+++ application/views/admin/remito/vedit.php
@@ -28,7 +28,7 @@
                                 <label for="fecha">Fecha de Recepción</label>
                                 <input type="text" id="txtfecha" name="txtfecha"  min="2020-01-01" max="2100-12-31" value="<?php echo !empty(form_error('txtfecha'))? set_value('txtfecha') :  date("d-m-Y", strtotime("$remitoedit->fecha"));?>" class= "form-control"   >
                             </div>
-                            <div class="col-sm-6 form-group">
+                            <div class="col-sm-12 form-group">
                                 <label for="observaciones">OBSERVACIONES</label>
                                 <input type="text" id="txtobservaciones" name="txtobservaciones" maxlength="1000" value="<?php echo !empty(form_error('txtobservaciones'))? set_value('txtobservaciones') : $remitoedit->observaciones ?>" class= "form-control"  >
                             </div>
