d75f29569482da93260514dff74193719631e458
Gonzalo Fonzalida <50595704+fonzalida@users.noreply.github.com>
2023-09-14 18:15:22 -0300

    fix

--- application/views/admin/recepcion_equipos/vprint.php
+++ application/views/admin/recepcion_equipos/vprint.php
@@ -49,7 +49,7 @@
                           <h3>Datos del Cliente</h3>
                                     <p>Cliente: <?php echo $model->Nombre; ?></p>
                                     <p>Domicilio: <?php echo $model->Domicilio." ".$model->Localidad." ".$model->Provincia; ?></p>
-                                    <p>Tel Mantenimiento: <?php echo $model->tel_mantenimiento; ?> &nbsp Tel Ventas: <?php echo $model->tel_ventas; ?> </p>
+                                    <p>Tel Mantenimiento: <?php echo $model->tel_mantenimiento; ?> &nbsp Tel Ventas: <?php echo $model->tel_venta; ?> </p>
                                     <p>Correo Electrónico: <?php echo $model->correo; ?></p>
                                 </div>
                                 <div class="equipment">
